696d7e9273cc2667ff8056dc75f4be43fe6bb5eb
pavel557 <Pawel.majer20@yandex.ru>
2022-05-28 12:42:40 +0300

    full models

--- app/Domain/Games/Models/Game.php
+++ app/Domain/Games/Models/Game.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Domain\Games\Models\Factories\GameFactory;
+use Database\Factories\GameFactory as FactoriesGameFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'gamedev_studio_id',
+    ];
+
+    public $timestamps = false;
+
+    public static function factory(): GameFactory
+    {
+        return GameFactory::new();
+    }
 }
