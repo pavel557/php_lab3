ae33b1fd0eacc7135e74879659a7dca06710d94e
pavel557 <Pawel.majer20@yandex.ru>
2022-05-28 11:23:46 +0300

    add models and migrations

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Ensi\LaravelServeSwagger\ServeSwaggerServiceProvider::class,
         /*
          * Application Service Providers...
          */
